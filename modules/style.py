bb94c9eb6ec78e6759b102c08314d0d6e0cfbd35
notsk11 <153056264+notsk11@users.noreply.github.com>
2024-03-24 17:42:39 +0530

    Create style.py

---
+++ modules/style.py
@@ -0,0 +1,141 @@
+css = """
+.gradio-container {
+  background-color: transparent;
+  left: -195px;
+}
+.image_out {
+  width: 1473px;
+  height: 600px;
+}
+.version {
+  left: 1790px;
+  top: 10px;
+  position: fixed !important;
+}
+.model {
+  left: 1510px;
+  top: 55px;
+  width: 300px;
+  height: 43px;
+  position: fixed !important;
+}
+.model-mark {
+  left: 1510px;
+  top: 31px;
+  position: fixed !important;
+}
+.scheduler {
+  left: 1510px;
+  top: 130px;
+  width: 300px;
+  height: 43px;
+  position: fixed !important;
+}
+.scheduler-mark {
+  left: 1510px;
+  top: 104px;
+  position: fixed !important;
+}
+.load-button {
+  left: 1820px;
+  top: 85px;
+  width: 80px;
+  height: 60px;
+  position: fixed !important; 
+}
+.style {
+  left: 1510px;
+  top: 205px;
+  width: 300px;
+  height: 43px;
+  position: fixed !important;
+}
+.style-mark {
+  left: 1510px;
+  top: 178px;
+  position: fixed !important;
+}
+.details {
+  left: 1510px;
+  top: 670px;
+  width: 380px;
+  height: 190px;
+  position: fixed !important;
+}
+.prompt {
+  width: 500px;
+  height: 80px;
+}
+.column1 {
+  width: 100px !important;
+}
+.height {
+  width: 300px !important;
+  left: 150px !important;
+  position: absolute !important;
+}
+.height-mark {
+  top: 0px;
+  left: 150px;
+}
+.width {
+  width: 300px !important;
+  top: 60px !important;
+  left: 150px !important;
+  position: absolute !important;
+}
+.width-mark {
+  top: 15px;
+  left: 150px;
+}
+.num_steps {
+  width: 300px !important;
+  left: 100px !important;
+  position: absolute !important;
+}
+.num-steps-mark {
+  top: 0px;
+  left: 100px;
+}
+.guidance {
+  width: 300px !important;
+  top: 60px;
+  left: 100px !important;
+  position: absolute !important;
+}
+.guidance-mark {
+  top: 15px;
+  left: 100px;
+}
+.num_images {
+  width: 300px !important;
+  top: 20px;
+  left: 50px !important;
+  position: absolute !important;
+}
+.num-images-mark {
+  top: 20px;
+  left: 50px;
+}
+.seed-inp {
+  top: 125px;
+  left: 515px;
+  width: 300px;
+  height: 40px;
+  position: absolute !important;
+}
+.seed-out {
+  top: 125px;
+  left: 830px;
+  width: 300px;
+  height: 40px;
+  position: absolute !important;
+}
+.generate {
+  top: 100px;
+  left: 1149px;
+  position: absolute !important;
+  width: 300px;
+  height: 70px;
+}
+"""
